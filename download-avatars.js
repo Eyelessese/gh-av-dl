67794885bee7f08fea74960f46fc109b77b46631
Ryan Ewanicke <reiz.talsean@gmail.com>
2018-09-03 22:07:53 -0600

    got getRepo kinda working, it writes to a json file but can't console log it propery for some reason.

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,26 @@
 const request = require('request');
+const getData = require('./request-handler').getData
+const fs = require('fs');
+let repoOptions =
+{
+  url: "https://api.github.com/repos/jquery/jquery/contributors",
+  headers:
+  {
+    'User-Agent': 'request'
+  }
+};
 
-console.log('test');
+function getRepoContributors(opt, cb)
+{
+  getData(opt, cb);
+}
+
+getRepoContributors(repoOptions, function (item)
+{
+  item.pipe(fs.createWriteStream('contributors.json'))
+  .on('finish', function ()
+  {
+    console.log('done');
+    console.log(fs.createReadStream('./contributors.json'));
+  });
+});
--- request-handler.js
+++ request-handler.js
@@ -1,28 +1,22 @@
 const request = require('request');
-const fs = require('fs');
 
-/*
-function getURL(protocol, domain, resource, com)
-{
-  return `${protocol}://${domain}${resource}.${com}`;
-}*/
 
-let url = 'https://sytantris.github.io/http-examples/future.jpg';
+function getData(opt, cb)
+{
+  request.get(opt)
+  .on('error', function (error)
+  {
+    console.log('error: ', error.statusCode, '\n', error.statusMessage);
+  })
+  .on('response', function (response)
+  {
 
-request.get(url)
-.on('error', function (error)
-{
-  console.log('error: ', error.statusCode, '\n', error.statusMessage);
-})
-.on('response', function (response)
-{
+    console.log('got response:', response.statusCode, '\n', response.statusMessage,'\n', response.headers['content-type']);
+    cb(response);
+  });
+}
 
-  console.log('got response:', response.statusCode, '\n', response.statusMessage,'\n', response.headers['content-type']);
-  console.log('download starting...');
-})
-.pipe(fs.createWriteStream('./futures.jpg')
-)
-.on('finish', function()
+module.exports =
 {
-  console.log('download complete');
-});
+getData: getData
+};
