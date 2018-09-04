7ab2a65d75ddb482c278eb1b74a4bf8ee024fdca
Ryan Ewanicke <reiz.talsean@gmail.com>
2018-09-03 18:49:18 -0600

    request=handler now has proper error and ok codes and download notifiers

--- request-handler.js
+++ request-handler.js
@@ -7,15 +7,22 @@ function getURL(protocol, domain, resource, com)
   return `${protocol}://${domain}${resource}.${com}`;
 }*/
 
-let url = 'https://sytantris.github.io/http-examples';
+let url = 'https://sytantris.github.io/http-examples/future.jpg';
 
 request.get(url)
 .on('error', function (error)
 {
-  console.log('error: ', error.statusCode);
+  console.log('error: ', error.statusCode, '\n', error.statusMessage);
 })
 .on('response', function (response)
 {
-  console.log('got response:', response.statusCode);
+
+  console.log('got response:', response.statusCode, '\n', response.statusMessage,'\n', response.headers['content-type']);
+  console.log('download starting...');
 })
-.pipe(fs.createWriteStream('./requesttest.html'));
+.pipe(fs.createWriteStream('./futures.jpg')
+)
+.on('finish', function()
+{
+  console.log('download complete');
+});
